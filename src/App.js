ef9d9cd23c3936d573cfd8b6189489a8a343026f
Amirreza Ghaffarian <amirghafarian7879@gmail.com>
2023-10-11 01:19:20 +0330

    0.5.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Main-Page/Navbar'
 import { Container } from 'react-bootstrap'
@@ -16,6 +18,23 @@ import Poisoningsymptoms from './components/Poisoningsymptoms-Page/Poisoningsymp
 import Rheumatology from './components/Rheumatology-Page/Rheumatology'
 
 const App = () => {
+  const [getApp, setApp] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // set loading
+        const { data: appData } = await axios.get("http://127.0.0.1:8000/api/v1/health/related-disease/list");
+        setApp(appData)
+
+        //set loading false
+      }
+      catch (err) {
+        console.log(err.message)
+        //set loading false
+      }
+    }
+    fetchData();
+  }, [])
   return (
     <>
       <BrowserRouter>
@@ -23,29 +42,30 @@ const App = () => {
           <Route
             path='/'
             element={
-              <Container style={{}}>
+              <Container style={ {} }>
                 <Navbar></Navbar>
                 <Board></Board>
-                <Aplications></Aplications>
+                <Aplications getApp={getApp}></Aplications>
                 <ContactUs></ContactUs>
               </Container>
             }
           />
 
           <Route path='/LogIn' element={
-            <Container style={{}}>
+            <Container style={ {} }>
               <Navbar></Navbar>
-          <LogIn></LogIn>
-          </Container>
+              <LogIn></LogIn>
+            </Container>
+
           } />
-          <Route path='/Endo' element={<Endo />} />
-          <Route path='/Hematology' element={<Hematology />} />
-          <Route path='/Infant' element={<Infant />} />
-          <Route path='/Medicaltoxicology' element={<Medicaltoxicology />} />
-          <Route path='/NewNephrology' element={<NewNephrology />} />
-          <Route path='/Poisoningsymptoms' element={<Poisoningsymptoms />} />
-          <Route path='/Rheumatology' element={<Rheumatology />} />
-          <Route path='*' element={<p>page not found</p>}/>
+          <Route path='/Endo' element={ <Endo /> } />
+          <Route path='/Hematology' element={ <Hematology /> } />
+          <Route path='/Infant' element={ <Infant /> } />
+          <Route path='/Medicaltoxicology' element={ <Medicaltoxicology /> } />
+          <Route path='/NewNephrology' element={ <NewNephrology /> } />
+          <Route path='/Poisoningsymptoms' element={ <Poisoningsymptoms /> } />
+          <Route path='/Rheumatology' element={ <Rheumatology /> } />
+          <Route path='*' element={ <p>page not found</p> } />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Main-Page/Aplications.jsx
+++ src/components/Main-Page/Aplications.jsx
@@ -1,15 +1,22 @@
 import { Container } from 'react-bootstrap'
+// import { useState } from 'react'
 import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-import { getApplication } from '../../data/items'
-import {
-  BiSolidNavigation
-} from "react-icons/bi";
-import "./Application.css"
-const Aplications = () => {
-  const Application = getApplication()
+// import { getApplication } from '../../data/items'
+import { BiSolidNavigation } from 'react-icons/bi'
+import './Application.css'
+
+
+
+const Aplications = ({ getApp }) => {
+
+
+
+
   return (
+
+
     <Container
       style={ {
         background: 'rgba(0, 0, 0, 0.45)'
@@ -17,19 +24,22 @@ const Aplications = () => {
       className=' p-5 rounded-5 mb-5 shadow-lg'
     >
       <Row xs={ 1 } md={ 3 } className='g-4'>
-        { Application.map(Application => (
-          <Col className='' key={ Application.id }>
+        { getApp.map(getApp => (
+
+          <Col className='' key={ getApp.name }>
             <Card align='center' className='bg-light rounded-4  shadow'>
               <Card.Img
                 style={ { width: '400px', height: '200px' } }
                 className=' rounded-4   img-fluid  my-0 p-2'
-                src={ `${Application.img}` }
+                // src={ `${Application.img}` }
+                src={ getApp.photo }
               />
               <Card.Body>
-                <Card.Title>{ `${Application.title}` }</Card.Title>
-                <Card.Text>{ `${Application.info}` }</Card.Text>
-                <button id="butapp" className='btn  rounded-5  mx-3 shadow' >
-                  Go to App<BiSolidNavigation className="mx-1 " />
+                <Card.Title>{ getApp.name }</Card.Title>
+                <Card.Text>{ getApp.about }</Card.Text>
+                <button id='butapp' className='btn  rounded-5  mx-3 shadow'>
+                  Go to App
+                  <BiSolidNavigation className='mx-1 ' />
                 </button>
               </Card.Body>
             </Card>
@@ -41,4 +51,3 @@ const Aplications = () => {
 }
 
 export default Aplications
-
